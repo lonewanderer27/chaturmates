df255270b81a65d17da7381826f70bbd7ebfa559
Adriane James Jabagat Puzon <adrianejames27@gmail.com>
2023-12-08 02:11:06 +0800

    Add getStudentById function to fetch student details, followers, following, and groups

---
+++ src/services/student/index.ts
@@ -0,0 +1,69 @@
+import { client } from "../../client";
+import { StudentResponse } from "../../types/student";
+
+export async function getStudentById(
+  id: string
+): Promise<StudentResponse['get']> {
+  console.log("student id: ", id);
+
+  // verify if the student exists
+  const student = await client
+    .from("students")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  // if student is not found, reject the promise
+  if (!student) {
+    return Promise.reject('Student not found')
+  }
+
+  // fetch the follower ids of the student from the database
+  const followerIds = await client
+    .from("student_followers")
+    .select("*")
+    .eq("following_id", id);
+
+  // fetch the followers of the student from the database
+  const followers = await client
+    .from("students")
+    .select("*")
+    .in("id", followerIds.data!.map((followerId) => followerId.follower_id));
+
+  // fetch the following ids of the student from the database
+  const followingIds = await client
+    .from("student_followers")
+    .select("*")
+    .eq("follower_id", id);
+
+  // fetch the following of the student from the database
+  const following = await client
+    .from("students")
+    .select("*")
+    .in("id", followingIds.data!.map((followingId) => followingId.following_id));
+
+  // fetch the group ids of the student from the database
+  const group_ids = await client
+    .from("group_members")
+    .select("*")
+    .eq("student_id", id);
+
+  // fetch the groups of the student from the database
+  const groups = await client
+    .from("groups")
+    .select("*")
+    .in("id", group_ids.data!.map((group_id) => group_id.group_id));
+
+  // return the student, followers, following, and groups
+  return Promise.resolve({
+    data: {
+      student: student.data!,
+      followers: followers.data!,
+      following: following.data!,
+      groups: groups.data!,
+    },
+    message: "Student found",
+    success: true,
+    error: null
+  })
+}

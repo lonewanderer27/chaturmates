b87caec88263a49a9f7f8abf10cdcdf7dd15ae84
Adriane James Jabagat Puzon <adrianejames27@gmail.com>
2023-12-08 02:16:56 +0800

    Add QueryClientProvider for React Query

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,21 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-supabase";
 import { client } from "./client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./theme/index.css";
 
 const container = document.getElementById("root");
+
+// instantiate a jotai root
 const root = createRoot(container!);
+
+// instantiate tanstack client
+const qClient = new QueryClient();
+
 root.render(
   <Provider value={client}>
-    <App />
+    <QueryClientProvider client={qClient}>
+      <App />
+    </QueryClientProvider>
   </Provider>
 );

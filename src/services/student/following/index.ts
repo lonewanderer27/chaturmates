dae8ea6458e6e7f90929a212fd3d14b9416d242d
Adriane James Jabagat Puzon <adrianejames27@gmail.com>
2023-12-08 02:11:12 +0800

    Add following functionality for students

---
+++ src/services/student/following/index.ts
@@ -0,0 +1,169 @@
+import { client } from "../../../client";
+import { StudentResponse } from "../../../types/student";
+
+export async function followStudent(
+  student_id: string,
+  following_id: string
+): Promise<StudentResponse["following"]["follow"]> {
+  // verify if the student exists
+  const studentFollower = await client
+    .from("students")
+    .select("*")
+    .eq("id", student_id)
+    .single();
+
+  // if the student is not found, reject the promise
+  if (!studentFollower) {
+    return Promise.reject("Student follower not found");
+  }
+
+  // verify if the student to be followed exists
+  const studentToFollow = await client
+    .from("students")
+    .select("*")
+    .eq("id", following_id)
+    .single();
+
+  // if the student is not found, reject the promise
+  if (!studentToFollow) {
+    return Promise.reject("Student to be followed not found");
+  }
+
+  // verify if the student is already following the student to be followed
+  const alreadyFollowing = await client
+    .from("student_followers")
+    .select("*")
+    .eq("follower_id", student_id)
+    .eq("following_id", following_id)
+    .single();
+
+  // if the student is already following the student to be followed, return an error
+  if (alreadyFollowing) {
+    return Promise.reject("You already follow this student");
+  }
+
+  // follow the student
+  const followed = await client
+    .from("student_followers")
+    .insert({
+      follower_id: Number(student_id),
+      following_id: Number(following_id),
+    })
+    .select("*")
+    .single();
+
+  // if the student could not be followed, reject the promise
+  if (!followed) {
+    return Promise.reject("Could not follow");
+  }
+
+  return Promise.resolve({
+    error: null,
+    message: "Followed successfully",
+    success: true,
+    data: {
+      student: followed.data!,
+    },
+  });
+}
+
+export async function unfollowStudent(
+  student_id: string,
+  following_id: string
+): Promise<StudentResponse["following"]["unfollow"]> {
+  // verify if the student exists
+  const studentFollower = await client
+    .from("students")
+    .select("*")
+    .eq("id", student_id)
+    .single();
+
+  // if the student is not found, reject the promise
+  if (!studentFollower) {
+    return Promise.reject("Student follower not found");
+  }
+
+  // verify if the student to be unfollowed exists
+  const studentToFollow = await client
+    .from("students")
+    .select("*")
+    .eq("id", following_id)
+    .single();
+
+  // if the student is not found, reject the promise
+  if (!studentToFollow) {
+    return Promise.reject("Student to be followed not found");
+  }
+
+  // verify if the student is not following the student to be unfollowed
+  const alreadyFollowing = await client
+    .from("student_followers")
+    .select("*")
+    .eq("follower_id", student_id)
+    .eq("following_id", following_id)
+    .single();
+
+  // if the student is not following the student to be unfollowed, return an error
+  if (!alreadyFollowing) {
+    return Promise.reject("You do not follow this student");
+  }
+
+  // unfollow a student
+  const unfollowed = await client
+    .from("student_followers")
+    .delete()
+    .eq("follower_id", Number(student_id))
+    .eq("following_id", following_id);
+
+  // if the student could not be unfollowed, reject the promise
+  if (!unfollowed) {
+    return Promise.reject("Could not unfollow");
+  }
+
+  return Promise.resolve({
+    error: null,
+    message: "Unfollowed successfully",
+    success: true,
+  });
+}
+
+export async function getAllStudentFollowings(
+  student_id: string
+): Promise<StudentResponse["following"]["getAll"]> {
+  // verify if the student exists
+  const student = await client
+    .from("students")
+    .select("*")
+    .eq("id", student_id)
+    .single();
+
+  if (!student) {
+    return Promise.reject("Student not found");
+  }
+
+  // fetch the following ids of the student from the database
+  const followingIds = await client
+    .from("student_followers")
+    .select("*")
+    .eq("follower_id", student_id);
+
+  // fetch the following of the student from the database
+  const following = await client
+    .from("students")
+    .select("*")
+    .in(
+      "id",
+      followingIds.data!.map((followingId) => followingId.following_id)
+    );
+
+  // return the student and their following
+  return Promise.resolve({
+    data: {
+      student: student.data!,
+      following: following.data!,
+    },
+    success: true,
+    message: "Fetched successfully",
+    error: null,
+  });
+}

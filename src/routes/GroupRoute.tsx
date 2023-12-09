7f9637522dc27876571d9e8aca6202897d048cb0
Adriane James Jabagat Puzon <adrianejames27@gmail.com>
2023-12-09 23:38:35 +0800

    Delete Klasmeyts page and route

--- src/routes/GroupRoute.tsx
+++ src/routes/GroupRoute.tsx
@@ -1,13 +1,11 @@
 import { IonRouterOutlet } from "@ionic/react";
 import { Route } from "react-router";
 import GroupPage from "../pages/GroupPage";
-import Klasmeyts from "../pages/Group/Klasmeyts";
 
 export default function GroupRoute() {
   return (
     <IonRouterOutlet id="group">
       <Route path="/group/:vanity_url" render={() => <GroupPage />} />
-      <Route path="/group/:vanity_url/students" render={() => <Klasmeyts />} />
     </IonRouterOutlet>
   );
 }

a310a6583131683e29dcb07bbf82368161b2395a
Adriane James Jabagat Puzon <adrianejames27@gmail.com>
2023-12-09 23:28:57 +0800

    Add new types for group chat URLs, group posts, and group comments

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,5 +9,9 @@ export type SearchHistoryType = Database["public"]["Tables"]["search_history"]["
 export type GroupType = Database["public"]["Tables"]["groups"]["Row"];
 export type GroupMemberType = Database["public"]["Tables"]["group_members"]["Row"];
 
+export type GroupChatUrlType = Database["public"]["Tables"]["group_chat_urls"]["Row"];
+export type GroupPostType = Database["public"]["Tables"]["group_posts"]["Row"];
+export type GroupCommentType = Database["public"]["Tables"]["group_comments"]["Row"];
+
 export type ThreadType = Database["public"]["Tables"]["threads"]["Row"];
 export type ThreadMessageType = Database["public"]["Tables"]["threads_messages"]["Row"];

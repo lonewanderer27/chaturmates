45ebc66a9f2dbea6edf3cf5d76ee7ecb3836d9db
Adriane James Jabagat Puzon <adrianejames27@gmail.com>
2023-12-12 05:20:12 +0800

    Refactor useSession hook

--- src/hooks/auth/useSession.tsx
+++ src/hooks/auth/useSession.tsx
@@ -1,42 +1,40 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { client } from "../../client";
 import { Session } from "@supabase/supabase-js";
 import { useAtom } from "jotai";
 import { passwordResetEmailAtom } from "../../atoms/auth";
 
 export default function useSession() {
-  const [passwordResetEmail, setPasswordResetEmail] = useAtom(passwordResetEmailAtom);
+  const [passwordResetEmail, setPasswordResetEmail] = useAtom(
+    passwordResetEmailAtom
+  );
   const [session, setSession] = useState<Session | null | undefined>(undefined);
-  const [nickname, setNickname] = useState<string | null | undefined>(undefined);
+  const [nickname, setNickname] = useState<string | null | undefined>(
+    undefined
+  );
 
   useEffect(() => {
     client.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-      setNickname(
-        session?.user.user_metadata.full_name.split(" ")[0]+
-        " "+session?.user.user_metadata.full_name.split(" ").slice(-1)[0]
-      )
-    })
+      setSession(session);
+      setNickname(session?.user.user_metadata.full_name.split(" ")[0]);
+    });
 
     const {
       data: { subscription },
     } = client.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-      setNickname(
-        session?.user.user_metadata.full_name.split(" ")[0]+
-        " "+session?.user.user_metadata.full_name.split(" ").slice(-1)[0]
-      )
+      setSession(session);
+      setNickname(session?.user.user_metadata.full_name.split(" ")[0]);
 
       if (_event === "PASSWORD_RECOVERY") {
-        setPasswordResetEmail(session!.user.email!)
+        setPasswordResetEmail(session!.user.email!);
       }
-    })
+    });
 
-    return () => subscription.unsubscribe()
-  }, [])
+    return () => subscription.unsubscribe();
+  }, []);
 
   return {
     session,
-    nickname
-  }
+    nickname,
+  };
 }

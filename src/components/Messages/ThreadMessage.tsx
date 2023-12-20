b1dbb6a343bc074b4bf21bacd683720b1f738525
Kil <biene.bryle.sanico@adamson.edu.ph>
2023-12-20 12:28:25 +0800

    timestamp added, still has problem with threads

--- src/components/Messages/ThreadMessage.tsx
+++ src/components/Messages/ThreadMessage.tsx
@@ -1,19 +1,30 @@
-import { IonCard, IonRow, IonText } from "@ionic/react";
-import { ThreadMessageType } from "../../types";
-import useSelfStudent from "../../hooks/student/useSelfStudent";
+import { useState } from 'react';
+import { IonCard, IonRow, IonText } from '@ionic/react';
+import { ThreadMessageType } from '../../types';
+import useSelfStudent from '../../hooks/student/useSelfStudent';
 
-export default function ThreadMessage(
-  props: ThreadMessageType & {
-    self: boolean;
-  }
-) {
+export default function ThreadMessage(props: ThreadMessageType & { self: boolean }) {
+  const [isCardClicked, setIsCardClicked] = useState(false);
+  const [timestamp, setTimestamp] = useState<string | null>(null);
+
+  const handleCardClick = () => {
+    setIsCardClicked(!isCardClicked);
+    setTimestamp(new Date(props.created_at).toLocaleTimeString());
+  };
+
   return (
-    <IonRow className={`flex ${props.self ? "justify-end" : "justify-start"}`}>
+    <IonRow className={`flex ${props.self ? 'justify-end' : 'justify-start'}`}>
       <IonCard
-        className={`my-2 ion-padding ${props.self ? "text-end" : "text-start"}`}
+        className={`my-2 ion-padding ${props.self ? 'text-end' : 'text-start'}`}
+        onClick={handleCardClick}
       >
         <IonText className="text-lg">{props.text}</IonText>
       </IonCard>
+      {isCardClicked && timestamp && (
+        <IonText className="timestamp" style={{ marginTop: '0px', marginLeft: '0px', marginRight: '10px'}}>
+          {timestamp}
+        </IonText>
+      )}
     </IonRow>
   );
 }
--- src/hooks/threads/useThread.tsx
+++ src/hooks/threads/useThread.tsx
@@ -1,31 +1,56 @@
 import { useEffect, useState } from "react";
 import { client } from "../../client";
 import { ThreadType } from "../../types";
+import useSelfStudent from "../student/useSelfStudent";
 
-export default function useThread(thread_id: number) {
+interface UserPressed {
+  id: profile_id; // Assuming you have some unique identifier for the user
+}
+
+export default function useThread(userPressed: UserPressed) {
   const [thread, setThread] = useState<ThreadType>();
 
-  function getThread() {
-    // fetch the thread
-    if (thread_id) {
-      client
-        .from("threads")
-        .select("*")
-        .eq("id", thread_id)
-        .single()
-        .then((response) => {
-          setThread(response.data as ThreadType);
-        });
+  function getOrCreateThread() {
+    if (!userPressed) {
+      return;
     }
+
+    
+    client
+      .from("threads")
+      .select("*")
+      .in("users", [useSelfStudent, userPressed.id])
+      .then((response) => {
+        const existingThread = response.data[0] as ThreadType;
+
+        if (existingThread) {
+        
+          setThread(existingThread);
+        } else {
+          
+          client
+            .from("threads")
+            .create({
+              users: [useSelfStudent, userPressed.id],
+            })
+            .then((newThreadResponse) => {
+              setThread(newThreadResponse.data as ThreadType);
+            })
+            .catch((error) => {
+              console.error("Error creating new thread:", error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching thread:", error);
+      });
   }
 
-  console.log("thread: ", thread);
-
   useEffect(() => {
-    getThread();
-  }, [thread_id])
+    getOrCreateThread();
+  }, [userPressed]);
 
   return {
-    thread
+    thread,
   };
 }

6d86adf47f939d23cbf1b00850523e24440c1203
lonewanderer27 <adrianejames27@gmail.com>
2024-01-16 22:28:37 +0800

    add vercel web analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import {createRoot} from "react-dom/client";
+import "./theme/index.css";
+
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+import { Analytics } from '@vercel/analytics/react';
 import App from "./App";
 import {Provider} from "react-supabase";
 import {client} from "./client";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import "./theme/index.css";
+import {createRoot} from "react-dom/client";
 
 // eslint-disable-next-line no-undef
 const container = document.getElementById("root");
@@ -19,6 +22,7 @@ root.render(
   <Provider value={client}>
     <QueryClientProvider client={qClient}>
       <App />
+      <Analytics />
     </QueryClientProvider>
   </Provider>
 );
